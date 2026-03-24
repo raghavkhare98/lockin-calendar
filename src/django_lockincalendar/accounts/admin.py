43eb31ab7191515e778e009529360c885c4f7a7a
raghavkhare98 <khare08rma@gmail.com>
2026-03-23 23:16:59 -0400

    Create CustomUserAdmin, and CustomUserCreation/Change forms

--- src/django_lockincalendar/accounts/admin.py
+++ src/django_lockincalendar/accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "is_staff",
+        "is_active",
+    ]
+
+admin.site.register(CustomUser, CustomUserAdmin)
