01bdbc281e169c606a30c1f0b72a8ac245aa807f
raghavkhare98 <khare08rma@gmail.com>
2026-04-04 20:32:31 -0400

    Add is_staff=True in LockinUserManager's superusercreation

--- src/django_lockincalendar/accounts/models.py
+++ src/django_lockincalendar/accounts/models.py
@@ -19,9 +19,11 @@ class LockinUserManager(BaseUserManager):
 
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_superuser", True)
-
+        extra_fields.setdefault("is_staff", True)
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
 
         return self._create_user(email, password, **extra_fields)
 
