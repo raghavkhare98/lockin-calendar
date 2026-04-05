c88725eef83a41ddab543e820aab8cd0998c9d2e
raghavkhare98 <khare08rma@gmail.com>
2026-04-04 20:26:38 -0400

    Fix bugs in LockinUserManager's create_superuser and create_user, add blank=True in user_photo in LockinUser

--- src/django_lockincalendar/accounts/models.py
+++ src/django_lockincalendar/accounts/models.py
@@ -4,28 +4,37 @@ from datetime import date
 
 class LockinUserManager(BaseUserManager):
     
-    def create_user(self, email, password, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError("The Email field must be set")
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self.__db)
+        user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_supseruser", True)
-        return self.create_superuser(email, password, **extra_fields)
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+
+        return self._create_user(email, password, **extra_fields)
 
 class LockinUser(AbstractUser):
     email = models.EmailField(unique=True)
     lockin_activities = models.CharField(max_length=128, default="")
-    user_photo = models.ImageField(upload_to="uploads/", null=True)
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    date_joined = models.DateTimeField(auto_now_add=True)
+    user_photo = models.ImageField(upload_to="uploads/", null=True, blank=True)
 
     objects = LockinUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
     @property
     def username(self):
