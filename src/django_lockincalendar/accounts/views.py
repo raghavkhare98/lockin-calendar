e00569358f973ead5a19ffef00271024e12181de
raghavkhare98 <khare08rma@gmail.com>
2026-04-05 20:18:58 -0400

    Add login template

--- src/django_lockincalendar/accounts/views.py
+++ src/django_lockincalendar/accounts/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class LoginSignupView(View):
+    template_name = 'login.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
--- src/django_lockincalendar/django_lockincalendar/urls.py
+++ src/django_lockincalendar/django_lockincalendar/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accounts.views import LoginSignupView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', LoginSignupView.as_view()),
 ]
