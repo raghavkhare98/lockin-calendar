f7fe0ce98887f21b37e0ff4f8fa38bcdcf6618d2
raghavkhare98 <khare08rma@gmail.com>
2026-04-28 16:27:41 -0400

    Create a base template

--- src/django_lockincalendar/django_lockincalendar/settings.py
+++ src/django_lockincalendar/django_lockincalendar/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'django_lockincalendar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'django_lockincalendar/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
