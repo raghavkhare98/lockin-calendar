309bd9c3f65a49c01cab985dcfb91e862b8a9124
raghavkhare98 <khare08rma@gmail.com>
2026-04-28 18:05:57 -0400

    Create navbar in base template

--- src/django_lockincalendar/django_lockincalendar/settings.py
+++ src/django_lockincalendar/django_lockincalendar/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #-- Project specific apps --,
-    'accounts'
+    'accounts',
+    'django_lockincalendar'
 ]
 
 MIDDLEWARE = [
